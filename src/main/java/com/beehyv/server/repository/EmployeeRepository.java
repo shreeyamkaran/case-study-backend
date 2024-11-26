ad20c832528f11d1189bedfd181eb91b984c5a90
Shreeyam Karan <shreeyam.karan@beehyv.com>
2024-11-26 23:56:51 +0530

    Spring Security Config

--- src/main/java/com/beehyv/server/repository/EmployeeRepository.java
+++ src/main/java/com/beehyv/server/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -43,4 +44,5 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
             nativeQuery = true)
     List<Long> findEmployeesIdsUnderManager(Long managerId);
 
+    Optional<Employee> findByUsername(String username);
 }
